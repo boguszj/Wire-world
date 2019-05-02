7edfa7ea0ef7424271746897dce99b2ef80320a3
SiwyKrzysiek <k.fajny@gmail.com>
2019-05-02 08:44:12 +0200

    Serialazacja na XML

--- Playground/src/json/Main.java
+++ Playground/src/json/Main.java
@@ -1,5 +1,7 @@
 package json;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import models.Board;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -12,18 +14,31 @@ public class Main {
 
         Board board = new Board(2, 3);
 
-        System.out.println(board.toString());
+        System.out.println("Board.toString():\n" + board.toString() + "\n");
 
 //        Board.State s = Board.State.CONDUCTOR;
 //        System.out.println(s);
 
+        String json;
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
         try {
-            objectMapper.writeValue(System.out, board);
+            json = objectMapper.writeValueAsString(board);
+            System.out.println("JSON:\n" + json + "\n");
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("Unable to serialize models");
+            System.out.println("Unable to serialize board to JSON");
+        }
+
+        String xml;
+        XmlMapper xmlMapper = new XmlMapper();
+        xmlMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+        try {
+            xml = xmlMapper.writeValueAsString(board);
+            System.out.println("XML:\n" + xml);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            System.out.println("Unable to serialize board to XML");
         }
     }
 }
