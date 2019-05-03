ad860a6965e394e487aa4155ce008b4cf3928c4e
SiwyKrzysiek <k.fajny@gmail.com>
2019-05-03 15:28:07 +0200

    Test rysowania planszy

--- Pre-code/WireWorld/src/controlers/GameOfLifeController.java
+++ Pre-code/WireWorld/src/controlers/GameOfLifeController.java
@@ -1,10 +1,18 @@
 package controlers;
 
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import models.GameOfLife;
+import views.CellularAutomatonView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 //TODO: States for GUI (Simulation paused, played, ...)
 
@@ -28,6 +36,9 @@ public class GameOfLifeController {
     private final Button saveButton;
     private final Button loadButton;
 
+    private GameOfLife gameOfLife;
+    private CellularAutomatonView cellularAutomatonView;
+
     public GameOfLifeController(Canvas canvas, Slider zoomSlider, ToggleButton autoRunToggleButton, Button previousGenerationButton, Button nextGenerationButton, Spinner widthSpinner, Spinner heightSpinner, Button randomButton, Button emptyButton, Button saveButton, Button loadButton) {
         this.canvas = canvas;
         this.zoomSlider = zoomSlider;
@@ -40,5 +51,26 @@ public class GameOfLifeController {
         this.emptyButton = emptyButton;
         this.saveButton = saveButton;
         this.loadButton = loadButton;
+
+        test();
+    }
+
+    private void test() {
+//        final GraphicsContext gc = canvas.getGraphicsContext2D();
+//
+//
+//        gc.setFill(Color.BLUE);
+//        gc.fillRect(0, 0, 100, 100);
+//        gc.fillRect(50, 0, 400, 400);
+        gameOfLife = new GameOfLife(6, 6);
+        gameOfLife.randomize();
+        Map<GameOfLife.CellStates, Paint> coloring = new HashMap<>();
+        coloring.put(GameOfLife.CellStates.DEAD, Color.BLACK);
+        coloring.put(GameOfLife.CellStates.ALIVE, Color.WHITE);
+
+
+        cellularAutomatonView = new CellularAutomatonView(gameOfLife, canvas, coloring);
+
+        cellularAutomatonView.draw();
     }
 }
