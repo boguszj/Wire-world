29d7d5f8591e76289741b694ff902f253151e74d
SiwyKrzysiek <k.fajny@gmail.com>
2019-05-03 12:54:57 +0200

    Prawie test gameOfLife.toString()

---
+++ Pre-code/WireWorld/tests/models/GameOfLifeTest.java
@@ -0,0 +1,19 @@
+package models;
+
+import models.GameOfLife;
+import models.GameOfLife.CellStates;
+
+public class GameOfLifeTest {
+    public static void main(String[] args) {
+        GameOfLife gameOfLife = new GameOfLife(2, 3);
+
+        CellStates[] states = {
+                CellStates.ALIVE, CellStates.ALIVE,
+                CellStates.DEAD, CellStates.DEAD,
+                CellStates.ALIVE, CellStates.DEAD
+        };
+        gameOfLife.setCells(states);
+
+        System.out.println(gameOfLife);
+    }
+}
